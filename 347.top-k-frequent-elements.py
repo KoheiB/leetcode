96adb93db03ac25788c4c4b74312fbfe71937b9c
Terashita Kohei <cwkterashita@terashitanoMacBook-Air-2.local>
2023-08-01 02:03:11 +0900

    add commentary

--- 347.top-k-frequent-elements.py
+++ 347.top-k-frequent-elements.py
@@ -5,16 +5,39 @@
 #
 
 # @lc code=start
+# class Solution:
+#     def topKFrequent(self, nums: [int], k: int) -> [int]:
+#         n_dict = {}  # number => count
+#         for n in nums:
+#             if n in n_dict:
+#                 n_dict[n] += 1
+#             else:
+#                 n_dict[n] = 1
+#         sorted_items = sorted(
+#             n_dict.items(), key=lambda item: item[1], reverse=True)
+#         result_dict = dict(sorted_items[:k])
+#         return result_dict
+
 class Solution:
     def topKFrequent(self, nums: [int], k: int) -> [int]:
-        n_dict = {}  # number => count
+        count = {}
+        freq = [[] for i in range(len(nums) + 1)]
+        print(freq)
+
         for n in nums:
-            if n in n_dict:
-                n_dict[n] += 1
-            else:
-                n_dict[n] = 1
-        sorted_items = sorted(
-            n_dict.items(), key=lambda item: item[1], reverse=True)
-        result_dict = dict(sorted_items[:k])
-        return result_dict
+            count[n] = 1 + count.get(n, 0)
+        for n, c in count.items():
+            freq[c].append(n)
+        print(freq)
+
+        res = []
+        for i in range(len(freq) - 1, 0, -1):
+            for n in freq[i]:
+                res.append(n)
+                print(res)
+                if len(res) == k:
+                    return res
+
+        # O(n)
+
 # @lc code=end
