aa6d592b3061ce235f0ecbefa079c206a5c888c2
Kohei Terashita <kou0704terasita@gmail.com>
2023-08-07 01:44:31 +0900

    22 739 clear

---
+++ 739.daily-temperatures.py
@@ -0,0 +1,26 @@
+#
+# @lc app=leetcode id=739 lang=python3
+#
+# [739] Daily Temperatures
+#
+
+# @lc code=start
+class Solution:
+    def dailyTemperatures(self, temperatures: [int]) -> [int]:
+        res = [0] * len(temperatures)
+        stack = []  # pair: [temp, index]
+
+        for i, t in enumerate(temperatures):
+            while stack and t > stack[-1][0]:
+                stackT, stackInd = stack.pop()
+                res[stackInd] = i - stackInd
+            stack.append((t, i))
+        return res
+
+
+
+# @lc code=end
+
+#stack 74, 71
+#
+Solution.dailyTemperatures(None, [73,74,75,71,69,72,76,73])
