015e0e230581b785a2112660ee75f04095661076
Kohei Terashita <kou0704terasita@gmail.com>
2023-08-08 02:06:16 +0900

    add commentary

--- 125.valid-palindrome.py
+++ 125.valid-palindrome.py
@@ -5,23 +5,44 @@
 #
 
 # @lc code=start
+# class Solution:
+#     def isPalindrome(self, s: str) -> bool:
+#         import re
+
+#         def remove_non_alphabetic(input_str):
+#             result_str = re.sub(r'[^a-zA-Z0-9]', '', input_str)
+#             return result_str
+#         _s = remove_non_alphabetic(s).lower()
+#         print(_s)
+#         if len(_s) <= 1:
+#             return True
+#         half_l = len(_s) // 2
+#         for i in range(half_l):
+#             if _s[i] == _s[(-i) -1]:
+#                 continue
+#             return False
+#         return True
 class Solution:
     def isPalindrome(self, s: str) -> bool:
-        import re
-
-        def remove_non_alphabetic(input_str):
-            result_str = re.sub(r'[^a-zA-Z0-9]', '', input_str)
-            return result_str
-        _s = remove_non_alphabetic(s).lower()
-        print(_s)
-        if len(_s) <= 1:
-            return True
-        half_l = len(_s) // 2
-        for i in range(half_l):
-            if _s[i] == _s[(-i) -1]:
-                continue
-            return False
+        l, r = 0, len(s) - 1
+        while l < r:
+            while l < r and not self.alphanum(s[l]):
+                l += 1
+            while l < r and not self.alphanum(s[r]):
+                r -= 1
+            if s[l].lower() != s[r].lower():
+                return False
+            l += 1
+            r -= 1
         return True
+
+    # Could write own alpha-numeric function
+    def alphanum(self, c):
+        return (
+            ord("A") <= ord(c) <= ord("Z")
+            or ord("a") <= ord(c) <= ord("z")
+            or ord("0") <= ord(c) <= ord("9")
+        )
 
 
 # @lc code=end
